8a260c9afa48d2e05e25be8271f1eaca232ceafd
Abdullahawan00119 <abdullahawan00119@gmail.com>
2025-05-27 14:48:20 -0700

    assignment completed

---
+++ app.js
@@ -0,0 +1,62 @@
+//                 Alert, Prompt, Console, and Concatenation
+//Assignment 1: Welcome Message
+// var userName = prompt("Please enter your name!");
+// alert("Welcome , "  +  userName + " !");
+
+//    Assignment 2: Age Message
+// var userName = prompt("Please Enter your Name!");
+// var userAge = prompt("Enter your Age!");
+// console.log(userName + " is " + userAge + " years old.");
+
+// Assignment 3: Favorite Color
+// var favoritColor = prompt("Enter your favorite color!");
+// alert("Wow! " + favoritColor + " is a nice color!");
+
+// Assignment 4: Simple Math Sentence
+// var userNumber1 = parseInt(prompt("Enter The first Number!"));
+// var userNumber2 = parseInt(prompt("Enter The second Number!"));
+// console.log(userNumber1 + " + " + userNumber2 + " is " + (userNumber1+userNumber2));
+
+
+// Assignment 5: About Me Summary
+// var userName = prompt("Enter your name!");
+// var userHobby = prompt("Enter your hobby!");
+// var userCountry = prompt("Enter your country name!");
+// alert("Hi, I'm " + userName + " from " + userCountry + ", and I love " + userHobby);
+
+
+//   Javascript String concatenation Practice:
+// Task 1: Create a Full Address
+// var userStreet = prompt("Enter your Street!");
+// var userCity = prompt("Enter your City!");
+// var userCountry = prompt("Enter your Country!");
+// alert("Your full address is : " + userStreet + ", " + userCity + ", " + userCountry);
+
+
+// Task 2: Make a Custom Username
+// var firstName = prompt("Enter your first name!");
+// var favoritNumber = prompt("Enter your favorite number!");
+// var userName = firstName + favoritNumber;
+// alert("Your new username is: " + userName);
+
+
+// Task 3: Email Generator
+// var firstName = prompt("Enter your first name!");
+// var lastName = prompt("Enter your last name!");
+// alert("Your Email is " + firstName + "." + lastName + "@gmail.com");
+
+
+// Task 4: Combine Favorite Things
+// "Your favorite things are: blue dogs and pizza!"
+// var color = prompt("please enter your favorite color!");
+// var animal = prompt("please enter your favorite animal!");
+// var food = prompt("please enter your favorite food!");
+// alert("your favorite things are : " + color + " , " + animal + " and " + food + "!");
+
+
+// Task 5: Quote with Author
+// "The only limit is your mind." - John Doe
+var quote = prompt("Enter the Quote");
+var autherName = prompt("Enter the author name!");
+alert(quote + ". " + "- " + autherName)
+
