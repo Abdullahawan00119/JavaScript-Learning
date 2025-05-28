8ad00b4f8000a3008fe39df0e119925ad29f908c
Abdullahawan00119 <abdullahawan00119@gmail.com>
2025-05-28 15:44:36 -0700

    logical operators

---
+++ Operators/app.js
@@ -0,0 +1,60 @@
+// Assignment Operators
+// =
+// -=
+// +=
+// *=
+// /=
+
+
+
+// var num = 20;
+
+// num += 30;
+// alert(num);
+
+
+// num *= 5;
+// console.log(num);
+
+// num -= 5;
+// console.log(num);
+
+
+// num /= 5;
+// num = num / 5;
+// console.log(num);
+
+// logical operators
+// && == and
+// || == OR
+
+// console.log(6 + 6 === 12 && 'yellow' === 'yellow' || 8 !== 8 && 3 * 3 === 9 || 'hello' !== 'HELLO');
+//             true && true || false && true || true
+//             true || false || true
+//         true
+
+
+// console.log( 'cat' !== 'dog' && 4 * 5 === 20 || 7 + 2 === 9 && 2 !== 1 || 12 / 3 === 4);
+// true && true || true && true  || true
+// true || true  || true
+// true
+
+
+console.log(true && 'hello' === 'hello' || 9 * 2 === 18 && 6 + 2 === 8 || false && 7 > 3);
+// true && true || true && true || false && true
+// true || true || false 
+// true
+
+
+
+
+
+
+
+
+
+
+
+
+
+
