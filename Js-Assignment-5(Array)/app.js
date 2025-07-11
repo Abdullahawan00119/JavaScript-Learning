68fb8be9ac6040bdd46d052e9668c4073e1ebadc
Abdullah@awan123 <abdullahawan00119@gmail.com>
2025-07-11 12:11:42 -0700

    Assignment 5 done

--- Js-Assignment-5(Array)/app.js
+++ Js-Assignment-5(Array)/app.js
@@ -1,1 +1,106 @@
-console.log('hello world');
+// 1. Create an array of 3 student names and add one more using push().
+// var studentsArray = ["Abdullah" , "Rehman" , "Jafar"];
+// studentsArray.push("Wasay" , "Rashid");
+// console.log(studentsArray);
+
+
+// 2. Remove the last item from an array of fruits using pop().
+// var fruitsArray = ["apple" , "banana" , "Peach"];
+// fruitsArray.pop();
+// console.log(fruitsArray);
+
+// 3. Add "Monday" to the beginning of a days array using unshift().
+// var weekDays = ["tuesday","wednesday" , "thrusday", "friday", "saturday","sunday"];
+// weekDays.unshift("Monday");
+// console.log(weekDays);
+
+// 4. Remove the first item from an array of colors using shift().
+// var colorArray = ["white" , "red" , "purple", "orange"];
+// colorArray.shift();
+// console.log(colorArray);
+//
+
+// 5. Use splice() to insert "Math" and "Science" at index 2 of a subjects array.
+// var  subjects = ["Computer","Philosophy","Envrion.","BioDiversity"];
+// subjects.splice(2,0,"Math" , "Science");
+// console.log(subjects);
+
+// 6. Use splice() to remove 2 elements starting from index 1 of a cities array.
+// var cities = ["Quetta","Karachi","Lahore","KPK"];
+// cities.splice(1,2);
+// console.log(cities);
+
+// 7. Use slice() to get the first 3 items from a countries array.
+// var countries = ["Pakistan", "Canada" ,"Iran","China","phalisteen","Turky"];
+//  var Splitcountries =  countries.slice(0,3);
+// console.log(Splitcountries);
+
+
+// 8. Convert the string "apple,banana,mango" into an array using split().
+// var striToArray = "apple,banana,mango";
+// console.log(striToArray.split());
+
+
+// 9. Join an array of characters ['J','S'] into a single string using join().
+// var arrayToString = ["J" , "S"];
+// console.log(arrayToString.join());
+
+// 10. Combine two arrays ['pen','pencil'] and ['eraser','sharpener'] using
+// concat().
+// var array1 = ['pen', 'pencil'];
+// var array2 = ['eraser', 'sharpener'];
+// var combineArray = array1.concat(array2);
+// console.log(combineArray);
+
+// 11. Sort an array of numbers [1,3,2,5,4,7,6] using sort() (as strings).
+// var arrayNumber = [1,3,2,5,4,7,6];
+// console.log(arrayNumber.sort().join());
+
+
+// 12. Sort an array of names alphabetically using sort().
+// var countries = ["Pakistan", "Canada" ,"Iran","China" , "Aliabad","phalisteen","Turky"];
+// console.log(countries.sort());
+
+// 13. Reverse the order of an array of numbers using reverse().
+// var numbersArray = [1,2,3,4,5,6,7,8,9,10];
+// console.log(numbersArray.reverse());
+
+
+// 14. Use indexOf() to find the index of "Blue" in a colors array.
+// var colorArray = ["white" , "red" , "purple", "orange" , "Blue"];
+// console.log(colorArray.indexOf(Blue));
+
+// 15. Add three new items to an empty array using push().
+// var emptyArray = [];
+// emptyArray.push("Ali" , "Rashid" , "Wasay")
+// console.log(emptyArray);
+
+// 16. Remove the middle item from an array of 5 numbers using splice().
+// var numbersArray = [1,2,3,4,5];
+// numbersArray.splice(2,1);
+// console.log(numbersArray);
+
+// 17. Create a string "HTML CSS JavaScript" and convert it to an array using split().
+// var webBasics = "HTML CSS JavaScript";
+// console.log(webBasics.split(" "));
+
+// 18. Create an array ['a', 'b', 'c'], reverse it, then join it with - in between.
+// var alphabets = ['a', 'b', 'c'];
+// console.log(alphabets.reverse().join("-"));
+
+// 19. Check if "Karachi" exists in an array using indexOf() and show its index.
+// var cities = ["Quetta","Karachi","Lahore","KPK"];
+// if(cities.includes("Karachi")){
+//     console.log(cities.indexOf("Karachi"));
+// }
+
+
+// 20. Concatenate two arrays of even and odd numbers using concat() and then reverse
+// them.
+// var evenNumber = [2,4,6,8,10];
+// var oddNumber = [1,3,5,7,9];
+// var combineNumbers = evenNumber.concat(oddNumber).sort((a,b)=> a-b).reverse();
+// console.log(combineNumbers);
+
+// here i used Compare function as .sort() sort by string not by numbers so 10 comes before 1 and after 2 so compare function correct it .
+
